2374ad2958c1e1d9404175c3a1bc26c686cac900
Manuel Guido <manuelguido.gb@gmail.com>
2025-12-15 14:59:25 -0300

    Agrega constants para colores

---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+/**
+ * Constantes de la aplicación
+ */
+
+// Colores
+export const COLORS = {
+	primary: "#4e64b1",
+	primary50: "#f3f6fb",
+	primary100: "#e4eaf5",
+	primary200: "#cedcef",
+	primary300: "#adc4e3",
+	primary400: "#86a6d4",
+	primary500: "#6989c8",
+	primary600: "#5671ba",
+	primary700: "#4e64b1",
+	primary800: "#42508b",
+	primary900: "#39456f",
+	primary950: "#262c45",
+
+	success: "#00a63e",
+	warning: "#e17100",
+	error: "#e7000b",
+};
